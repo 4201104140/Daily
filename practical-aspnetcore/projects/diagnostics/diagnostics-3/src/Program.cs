9909c45195dd4dbf0e25db0e868827cfbe6fc398
Phan Tấn Tài <phantantai9800@gmail.com>
2021-01-14 22:49:47 +0700

    tai

---
+++ practical-aspnetcore/projects/diagnostics/diagnostics-3/src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace PracticalAspNetCore
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+
+                })
+            })
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateWebHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>()
+                );
+    }
+}
